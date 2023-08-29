0cadeca0c3fb7284ca56ab4c55c50d312a23d85b
Carlos Dagorret <carlos@dagorret.com.ar>
2023-08-29 20:21:04 -0300

    To create migration

---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,10 @@
+<div
+    {{
+        $attributes->class([
+            'fi-logo text-xl font-bold leading-5 tracking-tight text-gray-950 dark:text-white',
+        ])
+    }}
+>
+    MT^3
+
+</div>
